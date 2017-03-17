38b63cfeadcd844c6794c9a9e11395ec4d7acce1
Yusuke Izawa <yuizalp@gmail.com>
2017-03-18 02:07:19 +0900

    add test of Church.scala

---
+++ app/src/test/scala/church/ChurchSpec.scala
@@ -0,0 +1,26 @@
+package church
+
+import org.scalatest._
+import church.Church._
+
+class ChurchSpec extends FlatSpec {
+
+  "Church Boolean" should "behave properly" in {
+    assert(and[Boolean](fls)(fls).toBoolean === false)
+    assert(or[Boolean](tru)(fls).toBoolean === true)
+    assert(not[Boolean](tru).toBoolean === false)
+    assert(test[Int](fls)(1)(2) === 2)
+  }
+
+  "Church Pair" should "behave properly" in {
+    val p1 = pair[Bool[Boolean]](tru)(fls)
+    val p2 = pair[Bool[Boolean]](tru)(fls)
+    assert(fst[Bool[Boolean]](p1).toBoolean === true)
+    assert(snd[Bool[Boolean]](p2).toBoolean === false)
+  }
+
+  "Church Number" should "behave properly" in {
+    assert(times[Int](succ(succ(zero)))(succ(zero)).toInt === 2)
+    assert(plus[Int](succ(succ(succ(zero))))(succ(succ(zero))).toInt === 5)
+  }
+}
